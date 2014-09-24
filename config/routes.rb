6ae32b678fdea6e4c3ea3648ebc43fc533f24b30
Marcos Siqueira Jr <junior.crvg@gmail.com>
2014-09-24 18:52:45 -0300

    g Subjects controller and config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :grades
+  resources :grades do
+    resources :subjects
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
