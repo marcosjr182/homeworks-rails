3086398cd60e2e9ec886a622913c8c3e03e64d4a
Marcos Siqueira Jr <junior.crvg@gmail.com>
2014-09-30 21:26:11 -0300

    Update Homework views and correct the destroy link

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -1,2 +1,35 @@
 class HomeworksController < ApplicationController
+  before_action :set_homework, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @grade = Grade.find(params[:grade_id])  
+    @subject = @grade.subjects.find(params[:subject_id])
+    @homework = @subject.homeworks.create(homework_params)
+    redirect_to grade_subject_path(@grade,@subject)
+  end
+  
+  def show
+  end
+
+  def edit
+  end 
+
+  def destroy
+    @grade = Grade.find(params[:grade_id])
+    @subject = @grade.subjects.find(params[:subject_id])
+    @homework = @subject.homeworks.find(params[:id])
+    @homework.destroy
+    redirect_to grade_subject_path(@grade,@subject)
+  end
+
+  private
+    def homework_params
+      params.require(:homework).permit(:name, :deadline)
+    end
+
+    def set_homework
+      @grade = Grade.find(params[:grade_id])
+      @subject = @grade.subjects.find(params[:subject_id])
+      @homework = @subject.homeworks.find(params[:id])
+    end
 end
